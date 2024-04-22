619a41f510abfe046018f88adabd522e2cb3be9b
nikfp <nik@nikfp.com>
2024-04-22 22:32:03 +0000

    more products stuff

--- lib/pento_web/components/core_components.ex
+++ lib/pento_web/components/core_components.ex
@@ -427,9 +427,9 @@ defmodule PentoWeb.CoreComponents do
 
   def header(assigns) do
     ~H"""
-    <header class={[@actions != [] && "flex items-center justify-between gap-6", @class]}>
+    <header class={[@actions != [] && "flex items-center justify-between gap-6", @class, "mb-4"]}>
       <div>
-        <h1 class="text-lg font-semibold leading-8 text-zinc-800">
+        <h1 class="text-2xl font-bold leading-8 text-zinc-800">
           <%= render_slot(@inner_block) %>
         </h1>
         <p :if={@subtitle != []} class="mt-2 text-sm leading-6 text-zinc-600">
